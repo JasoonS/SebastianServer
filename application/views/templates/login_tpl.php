e321cfc2340ef1dc0c49199f847644ccf29e0324
indranil <indranil.ray@eeshana.com>
2015-07-14 13:29:40 +0530

    login template added

---
+++ application/views/templates/login_tpl.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="utf-8">
+    <title>Login - Sebastian Admin Panel</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes"> 
+    
+	<link href="<?php echo THEME_ASSETS; ?>css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+	<link href="<?php echo THEME_ASSETS; ?>font-awesome/css/font-awesome.css" rel="stylesheet">
+    
+	<link href="<?php echo THEME_ASSETS; ?>css/style.css" rel="stylesheet" type="text/css">
+	
+</head>
+
+<body>
+<?php echo $body; ?>
+
+<script src="<?php echo THEME_ASSETS ?>js/jquery.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/bootstrap.js"></script>
+
+</body>
+
+</html>
+
+
