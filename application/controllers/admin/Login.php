e26ae02be131378ccc3654c88eb6f26dbfeb39e7
indranil <indranil.ray@eeshana.com>
2015-07-14 18:59:32 +0530

    Code added for login

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -7,21 +7,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller 
 {
-	public $data = array();
+	public	$data 					= array();
+	public	$validation_rules 		= array();
+	public	$login_flag				= FALSE;
+	private $logged_in_user_meta 	= array();
 	
 	public function __construct()
 	{
 		parent::__construct();
 	}
 	
-	/* Index method render
-	 * login page
+	/* Method render login page
 	 * @param void
 	 * return void
 	 */
 	public function index()
-	{		
+	{	
+		$this->data['action']	= "admin/login/verify_user";	
 		$this->template->load('login_tpl', 'login_vw',$this->data);
 	}
+	
+	/* Method authenticate user and verify user type
+	 * @param 	void
+	 * return  boolean TRUE/FALSE
+	 */
+	public function verify_user()
+	{
+		//Setting up validation rules for form inputs
+		$this->validation_rules = array(
+		array('field'=>'username','label'=>'Username','rules'=>'required','class'=>'text-danger'),
+		array('field'=>'password','label'=>'Password','rules'=>'required','class'=>'text-danger')
+		);
+		$this->form_validation->set_error_delimiters('<div class="text-danger">', '</div>');
+		$this->form_validation->set_rules($this->validation_rules);
+		
+		
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->data['action']	= "admin/login/verify_user";	
+			$this->template->load('login_tpl', 'login_vw',$this->data);
+		}else
+		{
+			// Authenticate user by performing below steps
+			//1. check if he is admin by checking user name and password
+			// if admin switch to admin dashboard
+			//2. Else it is hotel user and check for its hotel credentials
+			
+			die('process forms');
+		}
+			
+	}	
 }
 
--- application/views/login_vw.php
+++ application/views/login_vw.php
@@ -1,35 +1,36 @@
 <div class="account-container">
 	
 	<div class="content clearfix">
-		
-		<form action="#" method="post">
+		<form action="<?php echo base_url().$action?>" method="post">
 		
 			<h1>User Login</h1>		
 			
-			<div class="login-fields">
+			<div class="login-fields" id="idLoginFields">
 				
 				<p>Please provide your details</p>
 				
-				<div class="field">
+				<div class="field" id="idFields">
 					<label for="username">Username</label>
-					<input type="text" id="username" name="username" value="" placeholder="Username" class="login username-field" />
+					<input type="text" id="idUsername" name="username" value="<?php echo set_value('username');?>" placeholder="Username" class="login username-field" />
+					<?php echo form_error('username'); ?>
 				</div> <!-- /field -->
 				
 				<div class="field">
 					<label for="password">Password:</label>
-					<input type="password" id="password" name="password" value="" placeholder="Password" class="login password-field"/>
+					<input type="password" id="idPassword" name="password" value="<?php echo set_value('password');?>" placeholder="Password" class="login password-field"/>
+					<p class="text-danger"><?php echo form_error('password'); ?></p>
 				</div> <!-- /password -->
 				
 			</div> <!-- /login-fields -->
 			
 			<div class="login-actions">
 				
-				<span class="login-checkbox">
-					<input id="Field" name="Field" type="checkbox" class="field login-checkbox" value="First Choice" tabindex="4" />
+				<span class="login-checkbox" id="idLoginChkbox">
+					<input id="idRememberMe" name="Field" type="checkbox" class="field login-checkbox" value="First Choice" tabindex="4" />
 					<label class="choice" for="Field">Keep me signed in</label>
 				</span>
 									
-				<button class="button btn btn-success btn-large">Sign In</button>
+				<input type="submit" class="button btn btn-success btn-large" id="idSignIn" value="Sign In">
 				
 			</div> <!-- .actions -->
 			
