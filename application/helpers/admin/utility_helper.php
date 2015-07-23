a63a036346f306979bf29fbf84465007eb836d75
unknown <kalyani.joshi@eeshana.com>
2015-07-23 14:37:12 +0530

    Changes in  Utility Helper

--- application/helpers/admin/utility_helper.php
+++ application/helpers/admin/utility_helper.php
@@ -62,3 +62,90 @@ function getStateCities($state_id,$format='array')
 		return json_encode($citylist);
 	}
 }
+
+/* 
+	This function gives all available designation 
+*/
+function getAllDesignations($format='array')
+{
+	$CI =& get_instance();
+    $CI->load->model('Utility_model');
+	$designationlist=$CI->Utility_model->get_all_designations();
+	if($format == 'array')
+	{
+		return $designationlist;
+	}
+	else
+	{
+		return json_encode($designationlist);
+	}
+	
+}
+
+
+/* 
+	This function gives list of all available hotels 
+*/
+function getAllHotels($format='array')
+{
+	$CI =& get_instance();
+    $CI->load->model('Utility_model');
+	$hotellist=$CI->Utility_model->get_all_hotels();
+	if($format == 'array')
+	{
+		return $hotellist;
+	}
+	else
+	{
+		return json_encode($hotellist);
+	}
+	
+}
+
+
+
+/*
+	This function gives available User Types according to Logged in User Type.
+	If He is Super administrator He cannot see staff type(cannot add staff)
+*/
+function getAvailableHotelUserTypes($format='array')
+{
+   $CI = & get_instance();  //get instance, access the CI superobject
+   $CI->load->model('Utility_model');
+   $hotel_user_types=$CI->Utility_model->get_enum_values( 'sb_hotel_users', 'sb_hotel_user_type' );
+   if($format == 'array')
+			{
+				return  $hotel_user_types;
+			}
+			else
+			{
+				return json_encode( $hotel_user_types);
+			}	
+
+}
+
+/*
+	This function is used to Upload Image
+*/
+function upload_image($folderName)
+	{
+		$file_ext = substr(strrchr($_FILES[$folderName]['name'],'.'),1);
+		$name= time();
+		$config = array(
+				'upload_path' => "./user_data/$folderName",
+				'allowed_types' => "jpeg|jpg|png|gif",
+				'overwrite' => TRUE,
+				'file_name' => $name.".".$file_ext
+			);
+		$this->load->library('upload', $config);
+		if($this->upload->do_upload($folderName))
+		{
+			$data = array('upload_data' => $this->upload->data());
+			return $data['upload_data']['file_name'];
+		}
+		else
+		{
+			$error = array('error' => $this->upload->display_errors());
+			return $error;
+		}
+	}
