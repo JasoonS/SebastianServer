6208983f6df5c7b9fcccf16fe74e0e8e05cfa79c
indranil <indranil.ray@eeshana.com>
2015-07-28 14:22:22 +0530

    Top navigation created

--- application/views/templates/top_nav_tpl.php
+++ application/views/templates/top_nav_tpl.php
@@ -9,7 +9,7 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="">
                     <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                        <img src="<?php echo THEME_ASSETS;  ?>images/img.jpg" alt="">John Doe
+                        <img src="<?php echo THEME_ASSETS;  ?>images/img.jpg" alt=""><?php //echo $this->session-> ?>
                         <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
@@ -37,8 +37,8 @@
                     <ul id="menu1" class="dropdown-menu list-unstyled msg_list animated fadeInDown" role="menu">
                         <li>
                             <a>
-                                <span class="image">
-                            <img src="images/img.jpg" alt="Profile Image" />
+                            <span class="image">
+                            <img src="<?php echo THEME_ASSETS;  ?>images/img.jpg" alt="Profile Image" />
                         </span>
                                 <span>
                             <span>John Smith</span>
@@ -52,7 +52,7 @@
                         <li>
                             <a>
                                 <span class="image">
-                            <img src="images/img.jpg" alt="Profile Image" />
+                            <img src="<?php echo THEME_ASSETS;  ?>images/img.jpg" alt="Profile Image" />
                         </span>
                                 <span>
                             <span>John Smith</span>
@@ -66,7 +66,7 @@
                         <li>
                             <a>
                                 <span class="image">
-                            <img src="images/img.jpg" alt="Profile Image" />
+                            <img src="<?php echo THEME_ASSETS;  ?>images/img.jpg" alt="Profile Image" />
                         </span>
                                 <span>
                             <span>John Smith</span>
@@ -80,7 +80,7 @@
                         <li>
                             <a>
                                 <span class="image">
-                            <img src="images/img.jpg" alt="Profile Image" />
+                            <img src="<?php echo THEME_ASSETS;  ?>images/img.jpg" alt="Profile Image" />
                         </span>
                                 <span>
                             <span>John Smith</span>
