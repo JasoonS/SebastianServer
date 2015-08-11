b9b5cfb836c5cb6efbcf015266bc7dab1c1d05bb
indranil <indranil.ray@eeshana.com>
2015-08-11 12:12:09 +0530

    guest hotel user list view created

---
+++ application/views/hotel_guest_list_vw.php
@@ -0,0 +1,19 @@
+<div class="right_col" role="main">
+	<div class="">
+		<div class="page-title">
+            <div class="title_left">
+                <h3><?php echo $title; ?></h3>
+            </div>
+    	</div>
+        <div class="clearfix"></div>
+    </div>
+    <!-- footer content -->
+    <footer>
+        <div class="">
+            <p class="pull-right">Sebastian Admin |
+                <span class="lead"> <i class="fa fa-paw"></i></span>
+            </p>
+        </div>
+        <div class="clearfix"></div>
+    </footer>
+</div>
