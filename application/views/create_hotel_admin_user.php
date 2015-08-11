7809e25afd485c05b682f39c60ef080b096a3e73
unknown <kalyani.joshi@eeshana.com>
2015-08-11 13:10:35 +0530

    Updated Code

--- application/views/create_hotel_admin_user.php
+++ application/views/create_hotel_admin_user.php
@@ -268,10 +268,10 @@ $(function() {
 	 */
 	function populateChildServices(loggedusertype,userid,hotelid)
 		{
-			var proj_url=location.protocol + "//" + location.host;
+			
 			var creation_user_type=$("#sb_hotel_user_type").val();
 			var parent_service_id=$("#sb_parent_service_id").val();
-			var base_url = proj_url+'/sebastian-admin-panel/admin/ajax/get_ajax_data';
+			var base_url = proj_url+'/admin/ajax/get_ajax_data';
 			if(creation_user_type == 's'){
 				$("#child_services_control").show(2000);
 				$.ajax({
---
+++ assets/js/customjs/constants.js
@@ -0,0 +1,3 @@
+/*Project Location Path in javascript are defined here*/
+var proj_url		=	location.protocol + "//" + location.host+'/sebastian-admin-panel';
+//var proj_url		=	location.protocol + "//" + location.host+'/sebastian';
