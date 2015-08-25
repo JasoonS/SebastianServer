47ed7116ddbab1dbb03d70fc75bc1d875c3dee76
Abbas-eeshana <abbas.gabru@eeshana.com>
2015-08-25 15:45:42 +0530

    Hotel Rooms Creation View Updated

--- application/views/hotel_rooms_vw.php
+++ application/views/hotel_rooms_vw.php
@@ -1,5 +1,21 @@
-<script src="<?php echo THEME_ASSETS ?>js/bootstrap-formhelpers.min.js"></script>
-<script src="<?php echo THEME_ASSETS?>js/custom.js"></script>
+<link href="<?php echo THEME_ASSETS; ?>font-awesome/css/font-awesome.css" rel="stylesheet">    
+<link href="<?php echo THEME_ASSETS; ?>css/style.css" rel="stylesheet" type="text/css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<link href="<?php echo THEME_ASSETS; ?>css/star-rating.css" rel="stylesheet" type="text/css">
+<link href="<?php echo THEME_ASSETS; ?>css/bootstrap-toggle.css" rel="stylesheet" type="text/css">
+<link href="<?php echo THEME_ASSETS; ?>css/fileinput.css" rel="stylesheet" type="text/css">
+<link href="<?php echo THEME_ASSETS; ?>css/jquery-checktree.css" rel="stylesheet" type="text/css">
+<link href="<?php echo THEME_ASSETS; ?>css/jquery-ui.css" rel="stylesheet" type="text/css">
+<link href="<?php echo THEME_ASSETS; ?>css/jquery.dataTables.css" rel="stylesheet" type="text/css">
+<script src="<?php echo THEME_ASSETS ?>js/bootstrap.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/customjs/utility.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/star-rating.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/bootstrap-toggle.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/bootstrap-timepicker.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/fileinput.min.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/jquery-checktree.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/jquery-ui.js"></script>
+<script src="<?php echo THEME_ASSETS ?>js/jquery.dataTables.js"></script>
 <script>
  $("#room_num_prefix").on("keydown",function(e){return e.which !==32;});
  $("#room_num_postfix").on("keydown",function(e){return e.which !==32;});
@@ -28,36 +44,70 @@ function formvalidate()
 
 </script>
 <div class="right_col" role="main">
-<?php   echo $this->session->flashdata('rooms_success');
-					echo $this->session->flashdata('rooms_error'); ?>
     <div class="">
-	<h3>Create Room</h3>
-	
-	<div style="width:20%;margin:auto;padding:auto;">		
-		<form role="form" method="post" action="<?php   echo BASE_URL.$action ?>" onsubmit="return formvalidate()" class="" name="create_rooms_form">
-			<div class="form-group" style="">
-			<!-- <label for="room_num">Room No.:</label> -->
-			&nbsp;&nbsp;&nbsp;<br>
-			<label for="room_num_from">Room No.: From:</label>
-			<input type="text" class="form-control bfh-number" data-min="1" data-max="50" data-zeros="true" name="room_num_from" id="room_num_from" required>
-			</div>
-			<div class="form-group">
-			<label for="room_num_to">To:</label>
-			<input type="text" class="form-control bfh-number" data-min="1" data-max="50" data-zeros="true" name="room_num_to" id="room_num_to" required>
-			</div>
-			<div class="form-group">
-			<label for="room_num_prefix">Prefix</label>
-			<input type="text" class="form-control" name="room_num_prefix" id="room_num_prefix">
-			</div>
-			<div class="form-group">
-			<label for="room_num_postfix">Postfix</label>
-			<input type="text" class="form-control" name="room_num_postfix" id="room_num_postfix">
-			</div>
-			<!-- <div class="checkbox">
-			<label><input type="checkbox"> Remember me</label>
-			</div> -->
-			<input type="submit" class="btn btn-default" value="Submit"></input>
-		</form>
+    	<div class="page-title">
+            <div class="title_left">
+                <h3><?php echo $title ?></h3>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <h4 style="color:green"><?php   echo $this->session->flashdata('rooms_success'); ?> </h4>
+		<h4 style="color:red"><?php echo $this->session->flashdata('rooms_error'); ?></h4>
+	   <!--  <div class="">		 -->
+			<!-- <div style="width:20%;margin:auto;padding:auto;">	 -->	
+				<form role="form" method="post" action="<?php   echo BASE_URL.$action ?>" onsubmit="return formvalidate()" class="" name="create_rooms_form">
+					 <div class="row">
+	    			<div class = "col-md-6 col-xs-6 classFormBox">
+	    			<div class="x_panel classRequiredPanel">
+	    				<div class="x_title">
+		                    <h2><b>Mandatory Inputs</b></h2>	                            
+		                    <div class="clearfix"></div>
+		                </div>
+		                <div class = "x_content">
+
+                		<div class = "form-group classFormInputsBox">
+					<!-- <div class="form-group" style="">
+					<label for="room_num">Room No.:</label>
+					&nbsp;&nbsp;&nbsp;<br> -->
+							<label for="room_num_from" class="col-xs-4 control-label">Room No.: From:</label>
+							<div class="col-xs-2">
+							<input type="text" class="form-control" data-min="1" data-max="50" data-zeros="true" name="room_num_from" id="room_num_from" required>
+							</div>
+						<!-- </div>	
+						<div class = "form-group classFormInputsBox"> -->
+
+							<label for="room_num_to" class="col-xs-1 control-label">To:</label>
+							<div class="col-xs-2">
+							<input type="text" class="form-control" data-min="1" data-max="50" data-zeros="true" name="room_num_to" id="room_num_to" required>
+							</div>
+						</div>
+						<br/><br/>
+						<div class = "form-group classFormInputsBox">
+							<label for="room_num_prefix" class="col-xs-4 control-label">Prefix</label>
+							<div class="col-xs-6">
+							<input type="text" class="form-control" name="room_num_prefix" id="room_num_prefix">
+							</div>
+						</div>
+						<br/><br/>
+						<div class = "form-group classFormInputsBox">
+							<label for="room_num_postfix" class="col-xs-4 control-label">Postfix</label>
+							<div class="col-xs-6">
+							<input type="text" class="form-control" name="room_num_postfix" id="room_num_postfix">
+							</div>
+						</div>
+						<br/><br/>
+					<!-- <div class="checkbox">
+					<label><input type="checkbox"> Remember me</label>
+					</div> -->
+					<!-- <input type="submit" class="btn btn-default" value="Submit"></input> -->
+						<div class="control-group">
+							<div class="controls">
+								<input type="submit" class="btn btn-primary btn-lg btn-block" value="Submit"></input>
+								<!-- <button id="submit"  class="btn btn-primary btn-lg btn-block">Upload Image</button> -->
+							</div>
+						</div>
+				</form>
+			<!-- </div>
+		</div> -->
 	</div>
-</div>
 </div>
