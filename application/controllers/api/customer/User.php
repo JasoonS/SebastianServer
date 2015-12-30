f01bc1b9271608ad381967528a76095caa001bb6
Jason Smythe <jason@smythemail.za.net>
2015-12-30 22:28:49 +0200

    add printing to all the user details

--- application/controllers/api/customer/User.php
+++ application/controllers/api/customer/User.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //   THIS IS API FOR HOTEL USERS(CUSTOMERS). THIS IS CUSTOMER SIDE API.THIS API WILL FOCUS ON LOGIN/LOGOUT/CHANGE PASSWORD/FORGOT PASSWORD/NOTIFICATIONS FOR HOTEL USER(CUSTOMER).
 
 if( ! defined('BASEPATH')) exit('No direct script access allowed');
@@ -8,9 +8,9 @@ class User extends CI_Controller
 	function __construct()
 	{
 		header('Access-Control-Allow-Origin: *');
-			
+
 		parent::__construct();
-		
+
 		/*
 			this code is to maintain all hits log
 			as well as to restrict API to any devices not browsers
@@ -32,49 +32,66 @@ class User extends CI_Controller
 	/**
 	 * This function will take reservation id and device token etc. check for reservation id updates
 	 * device token and return User details as well as hotel's services list etc
-	 * return type- 
+	 * return type-
 	 * created on - 20th July 2015;
-	 * updated on - 
+	 * updated on -
 	 * update 	  -
 	 * created by - Akshay Patil;
 	 */
 	function login($sb_guest_reservation_code,$cdt_token,$cdt_deviceType,$cdt_macid)
 	{
-		// $sb_guest_reservation_code = 	$this->input->post('sb_guest_reservation_code');
+    // $sb_guest_reservation_code = 	$this->input->post('sb_guest_reservation_code');
 		// $cdt_token				= 	$this->input->post('cdt_token');
 		// $cdt_deviceType		    =   $this->input->post('cdt_deviceType');
 		// $cdt_macid 				= 	$this->input->post('cdt_macid');
-		
-		if($cdt_token == '(null)' || $cdt_token == 'null' || $cdt_token == null || $cdt_token == '')
-			$cdt_token = '';
-		if(strlen($cdt_token) < '10')
-			$cdt_token = '';
-
-		if($sb_guest_reservation_code == '' ||  $cdt_deviceType == ''|| $cdt_macid=='' )
+		if( null == $cdt_token )
 		{
-			response_fail("Please Insert All data correctly");
-
-		}
-		else
-		{
-			$user_info = $this->User_model->login($sb_guest_reservation_code , $cdt_token, $cdt_deviceType ,$cdt_macid);
+			log_message('ERROR', 'User didnt supply a cdt_token');
+			$user_info = $this->User_model->loginNotoken($sb_guest_reservation_code);
 			if($user_info == 0)
 			{
+				log_message('ERROR', 'The user was not found');
 				response_fail("User Not Found");
 			}
 			else
 			{
+				log_message('ERROR', 'The user was found, info returned.');
 				response_ok($user_info);
+			}
+		}
+		else
+		{
+			if($cdt_token == '(null)' || $cdt_token == 'null' || $cdt_token == null || $cdt_token == '')
+				$cdt_token = '';
+			if(strlen($cdt_token) < '10')
+				$cdt_token = '';
+
+			if($sb_guest_reservation_code == '' ||  $cdt_deviceType == ''|| $cdt_macid=='' )
+			{
+				response_fail("Please Insert All data correctly");
+
+			}
+			else
+			{
+				$user_info = $this->User_model->login($sb_guest_reservation_code , $cdt_token, $cdt_deviceType ,$cdt_macid);
+				if($user_info == 0)
+				{
+					response_fail("User Not Found");
+				}
+				else
+				{
+					response_ok($user_info);
+				}
 			}
 		}
 	}
 
 	/**
-	 * This function will take reservation id, check for reservation id 
+	 * This function will take reservation id, check for reservation id
 	 * and return User details etc
-	 * return type- 
+	 * return type-
 	 * created on - 20th July 2015;
-	 * updated on - 
+	 * updated on -
 	 * created by - Akshay Patil;
 	 */
 	function get_userDetails()
@@ -105,27 +122,35 @@ class User extends CI_Controller
 
 	/**
 	 * This function is for forgot Reservation code.
-	 * return type- 
+	 * return type-
 	 * created on - 21th July 2015;
-	 * updated on - 
+	 * updated on -
 	 * created by - Samrat Aher;
 	 */
 	function forgot()
 	{
-		$sb_guest_firstName = $this->input->post('sb_guest_firstName');
+    log_message('ERROR', 'forgot(): ');
+    $postParams = $this->input->post();
+    $string = '';
+    foreach ($postParams as $key => $val) {
+        $string .= ' key:'.$key.', value:'.$val.';';
+    }
+    log_message('ERROR', $string);
+    log_message('ERROR', 'forgot() done:');
+    $sb_guest_firstName = $this->input->post('sb_guest_firstName');
 		$sb_guest_lastName = $this->input->post('sb_guest_lastName');
 		$sb_guest_email = $this->input->post('sb_guest_email');
 		$sb_hotel_name = $this->input->post('sb_hotel_name');
 		$sb_guest_contact_no = $this->input->post('sb_guest_contact_no');
 
-		if ($sb_guest_firstName == '' || $sb_guest_lastName == '' || $sb_guest_email == '' || $sb_hotel_name == '' || $sb_guest_contact_no == '' ) 
+		if ($sb_guest_firstName == '' || $sb_guest_lastName == '' || $sb_guest_email == '' || $sb_hotel_name == '' || $sb_guest_contact_no == '' )
 		{
 			response_fail("Please insert all the fields");
 		}
 		else
 		{
 			$resp = $this->User_model->get_reservation($sb_guest_firstName, $sb_guest_lastName , $sb_guest_email, $sb_hotel_name , $sb_guest_contact_no);
-			
+
 			if (empty($resp))
 			{
 				response_fail("No such user exist");
@@ -143,21 +168,21 @@ class User extends CI_Controller
 				$this->load->helper('admin/utility_helper');
 				sendMail('',$sb_guest_email,"Reservation code",$body);
 				response_ok();
-			}				
+			}
 		}
 	}
 
 
 	public function get_hotel_names()
-	{					
+	{
 		$sb_hotel_name = '';
-		if (!empty($this->input->post())) 
+		if (!empty($this->input->post()))
 		{
 			$sb_hotel_name = $this->input->post('sb_hotel_name');
 		}
 
 		$result = $this->User_model->get_hotel($sb_hotel_name);
-		if (!empty($result)) 
+		if (!empty($result))
 		{
 			$resp = array(
 	    	        'result' =>$result
@@ -169,15 +194,15 @@ class User extends CI_Controller
 			response_fail("No Hotel Exists");
 		}
 	}
-	
-	public function get_hotel_name_from_id() 
+
+	public function get_hotel_name_from_id()
 	{
 		// add imput sanitization (ie post must have hotel_id property otherwise error)
 		$sb_hotel_id = $this->input->post("sb_hotel_id");
 
 		$result = $this->User_model->get_hotel_name($sb_hotel_id);
-		
-		if (!empty($result)) 
+
+		if (!empty($result))
 		{
 			response_ok($result[0]);
 		}
@@ -190,14 +215,22 @@ class User extends CI_Controller
 
 	/**
 	 * This function is for new Signup/login process.
-	 * return type- 
+	 * return type-
 	 * created on - 08th Sept 2015;
-	 * updated on - 
+	 * updated on -
 	 * created by - Akshay PAtil;
 	 */
 	public function signup()
 	{
-		$sb_guest_firstName = 	$this->input->post('sb_guest_firstName');
+    log_message('ERROR', 'signup(): ');
+    $postParams = $this->input->post();
+    $string = '';
+    foreach ($postParams as $key => $val) {
+        $string .= ' key:'.$key.', value:'.$val.';';
+    }
+    log_message('ERROR', $string);
+    log_message('ERROR', 'signup() done:');
+    $sb_guest_firstName = 	$this->input->post('sb_guest_firstName');
 		$sb_guest_lastName 	= 	$this->input->post('sb_guest_lastName');
 		$sb_guest_email 	= 	$this->input->post('sb_guest_email');
 		$sb_hotel_id 		= 	$this->input->post('sb_hotel_id');
@@ -212,13 +245,17 @@ class User extends CI_Controller
 		$cdt_macid			= 	$this->input->post('cdt_macid');
 
 		$check_reservation =  $this->User_model->check_reservation($sb_guest_email, $sb_hotel_id);
+		log_message('ERROR', 'The user is () due to the following count.');
+		log_message('ERROR', count($check_reservation));
 		if(count($check_reservation)>0)
 		{
+			log_message('ERROR', 'activated');
 			$sb_guest_reservation_code = $check_reservation[0]['sb_guest_reservation_code'];
 			$this->login($sb_guest_reservation_code,$cdt_token,$cdt_deviceType,$cdt_macid);
 		}
 		else
 		{
+			log_message('ERROR', 'Not activated');
 			$check_visitor = $this->User_model->check_visitor($sb_guest_email, $sb_hotel_id);
 			if(count($check_visitor)>0)
 			{
@@ -257,7 +294,15 @@ class User extends CI_Controller
 
 	public function logout()
 	{
-		$sb_hotel_guest_booking_id 	= 	$this->input->post('sb_hotel_guest_booking_id');
+    log_message('ERROR', 'logout(): ');
+    $postParams = $this->input->post();
+    $string = '';
+    foreach ($postParams as $key => $val) {
+        $string .= ' key:'.$key.', value:'.$val.';';
+    }
+    log_message('ERROR', $string);
+    log_message('ERROR', 'logout() done:');
+    $sb_hotel_guest_booking_id 	= 	$this->input->post('sb_hotel_guest_booking_id');
 		$cdt_macid 		= 	$this->input->post('cdt_macid');
 
 		if($cdt_macid == '' || $sb_hotel_guest_booking_id == '')
@@ -267,7 +312,7 @@ class User extends CI_Controller
 		else
 		{
 			$updateData = array(
-					"cdt_token" => "",				
+					"cdt_token" => "",
 				);
 			$data = $this->User_model->logout($sb_hotel_guest_booking_id,$cdt_macid,$updateData);
 			response_ok();
@@ -276,7 +321,15 @@ class User extends CI_Controller
 
 	public function hotel_customer()
 	{
-		// print_r($_POST); die();	
+    log_message('ERROR', 'hotel_customer(): ');
+    $postParams = $this->input->post();
+    $string = '';
+    foreach ($postParams as $key => $val) {
+        $string .= ' key:'.$key.', value:'.$val.';';
+    }
+    log_message('ERROR', $string);
+    log_message('ERROR', 'hotel_customer() done:');
+    // print_r($_POST); die();
 		$sb_guest_firstName = 	$this->input->post('sb_guest_firstName');
 		$sb_guest_lastName 	= 	$this->input->post('sb_guest_lastName');
 		$sb_guest_email 	= 	$this->input->post('sb_guest_email');
@@ -309,8 +362,8 @@ class User extends CI_Controller
 				"sb_guest_reservation_code" =>$sb_guest_reservation_code
 				);
 			$sb_hotel_guest_booking_id = $this->User_model->register_visitor($insert_arr);
-			
-			
+
+
 			if($sb_hotel_guest_booking_id != 0)
 			{
 				$data = $this->login($sb_guest_reservation_code,$cdt_token,$cdt_deviceType,$cdt_macid);
@@ -321,8 +374,5 @@ class User extends CI_Controller
 				response_fail("ErrorCode#2,Something Went Wrong");
 			}
 		}
-		
 	}
-
-	
-}	
+}
--- application/models/api/customer/User_model.php
+++ application/models/api/customer/User_model.php
@@ -4,17 +4,17 @@ class User_model extends CI_Model
 
 	function get_userDetails($sb_guest_reservation_code)
 	{
-		$qry = "SELECT h.sb_hotel_name, gb.sb_guest_firstName, gb.sb_guest_lastName FROM `sb_hotel_guest_bookings` as gb 
-				JOIN sb_hotels as h on gb.`sb_hotel_id` = h.sb_hotel_id 
-				JOIN sb_hotel_guest_reservation_attributes as gba on gba.`sb_guest_reservation_code` = gb.`sb_guest_reservation_code` 
+		$qry = "SELECT h.sb_hotel_name, gb.sb_guest_firstName, gb.sb_guest_lastName FROM `sb_hotel_guest_bookings` as gb
+				JOIN sb_hotels as h on gb.`sb_hotel_id` = h.sb_hotel_id
+				JOIN sb_hotel_guest_reservation_attributes as gba on gba.`sb_guest_reservation_code` = gb.`sb_guest_reservation_code`
 				where gb.`sb_guest_reservation_code` = '$sb_guest_reservation_code'";
 		$query = $this->db->query($qry);
 		return $query->result_array();
 	}
-	
+
 	function login($sb_guest_reservation_code , $cdt_token, $cdt_deviceType ,$cdt_macid)
 	{
-		$qry = "SELECT * FROM `sb_hotel_guest_bookings` 
+		$qry = "SELECT * FROM `sb_hotel_guest_bookings`
 				LEFT JOIN `sb_hotel_guest_reservation_attributes`
 				ON `sb_hotel_guest_reservation_attributes`.`sb_guest_reservation_code` = `sb_hotel_guest_bookings`.`sb_guest_reservation_code`
 				where `sb_hotel_guest_bookings`.`sb_guest_reservation_code` = '$sb_guest_reservation_code'";
@@ -22,31 +22,31 @@ class User_model extends CI_Model
 		$query = $this->db->query($qry);
 		$custData = $query->result_array();
 		/*$roomNumbers = array();
-		for ($i=0; $i < count($custData); $i++) { 
+		for ($i=0; $i < count($custData); $i++) {
 			array_push($roomNumbers,$custData[$i]['sb_guest_allocated_room_no']);
 		}*/
-		
+
 		if(count($custData)>0)
 		{
 			$sb_hotel_id =$custData[0]['sb_hotel_id'];
 			$roomNumbers = array();
-			$sql4 = "SELECT `sb_guest_allocated_room_no` 
-					FROM `sb_hotel_guest_reservation_attributes` 
-					WHERE `sb_guest_reservation_code` = '$sb_guest_reservation_code' 
+			$sql4 = "SELECT `sb_guest_allocated_room_no`
+					FROM `sb_hotel_guest_reservation_attributes`
+					WHERE `sb_guest_reservation_code` = '$sb_guest_reservation_code'
 					AND sb_guest_actual_check_out ='0000-00-00 00:00:00'";
 			$query4 = $this->db->query($sql4);
 			$roomData = $query4->result_array();
-			for ($i=0; $i < count($roomData); $i++) { 
+			for ($i=0; $i < count($roomData); $i++) {
 				array_push($roomNumbers,$roomData[$i]['sb_guest_allocated_room_no']);
 			}
-			/*$sql1 = "SELECT * FROM `sb_hotel_parent_services` WHERE `sb_parent_service_id` 
+			/*$sql1 = "SELECT * FROM `sb_hotel_parent_services` WHERE `sb_parent_service_id`
 					in(SELECT distinct(`sb_parent_service_id`) FROM `sb_hotel_service_map` WHERE `sb_hotel_id` = '$sb_hotel_id')";
 			*/
 			$IMP_PATH = PARENT_SERVICE_PIC;
 			$sql1 = "SELECT `sb_parent_service_id`,`sb_parent_service_name`,
 					CONCAT('$IMP_PATH',`sb_parent_service_image`) as `sb_parent_service_image`,`sb_parent_service_color`,
-					`sb_parent_service_created_on` 
-					FROM `sb_hotel_parent_services` 
+					`sb_parent_service_created_on`
+					FROM `sb_hotel_parent_services`
 					WHERE `sb_parent_service_id` in(SELECT distinct(`sb_parent_service_id`) FROM `sb_hotel_service_map` WHERE `sb_hotel_id` = '$sb_hotel_id')";
 			$query = $this->db->query($sql1);
 			$services = $query->result_array();
@@ -65,7 +65,7 @@ class User_model extends CI_Model
 				`sb_hotel_category`,`sb_hotel_created_on`,`sb_hotel_website`,
 				CONCAT('$IMP_PATH',`sb_hotel_pic`) as `sb_hotel_pic`,
 				`sb_hotel_owner`, `sb_property_built_month` ,`sb_hotel_email`,`sb_property_built_year`,`sb_property_open_year`,
-				`is_active` 
+				`is_active`
 				FROM sb_hotels WHERE `sb_hotel_id` = '$sb_hotel_id';";
 			$query = $this->db->query($sql1);
 			$hotel = $query->result_array();
@@ -105,6 +105,98 @@ class User_model extends CI_Model
 		}
 	}
 
+	function loginNotoken($sb_guest_reservation_code)
+	{
+		$qry = "SELECT * FROM `sb_hotel_guest_bookings`
+				LEFT JOIN `sb_hotel_guest_reservation_attributes`
+				ON `sb_hotel_guest_reservation_attributes`.`sb_guest_reservation_code` = `sb_hotel_guest_bookings`.`sb_guest_reservation_code`
+				where `sb_hotel_guest_bookings`.`sb_guest_reservation_code` = '$sb_guest_reservation_code'";
+				//AND `sb_hotel_guest_reservation_attributes`.sb_guest_actual_check_out ='0000-00-00 00:00:00'";
+		$query = $this->db->query($qry);
+		$custData = $query->result_array();
+		/*$roomNumbers = array();
+		for ($i=0; $i < count($custData); $i++) {
+			array_push($roomNumbers,$custData[$i]['sb_guest_allocated_room_no']);
+		}*/
+
+		if(count($custData)>0)
+		{
+			$sb_hotel_id =$custData[0]['sb_hotel_id'];
+			$roomNumbers = array();
+			$sql4 = "SELECT `sb_guest_allocated_room_no`
+					FROM `sb_hotel_guest_reservation_attributes`
+					WHERE `sb_guest_reservation_code` = '$sb_guest_reservation_code'
+					AND sb_guest_actual_check_out ='0000-00-00 00:00:00'";
+			$query4 = $this->db->query($sql4);
+			$roomData = $query4->result_array();
+			for ($i=0; $i < count($roomData); $i++) {
+				array_push($roomNumbers,$roomData[$i]['sb_guest_allocated_room_no']);
+			}
+			/*$sql1 = "SELECT * FROM `sb_hotel_parent_services` WHERE `sb_parent_service_id`
+					in(SELECT distinct(`sb_parent_service_id`) FROM `sb_hotel_service_map` WHERE `sb_hotel_id` = '$sb_hotel_id')";
+			*/
+			$IMP_PATH = PARENT_SERVICE_PIC;
+			$sql1 = "SELECT `sb_parent_service_id`,`sb_parent_service_name`,
+					CONCAT('$IMP_PATH',`sb_parent_service_image`) as `sb_parent_service_image`,`sb_parent_service_color`,
+					`sb_parent_service_created_on`
+					FROM `sb_hotel_parent_services`
+					WHERE `sb_parent_service_id` in(SELECT distinct(`sb_parent_service_id`) FROM `sb_hotel_service_map` WHERE `sb_hotel_id` = '$sb_hotel_id')";
+			$query = $this->db->query($sql1);
+			$services = $query->result_array();
+			if(count($services) == 0)
+			{
+				$service =array();
+			}
+			else
+			{
+				$service = $services;
+			}
+			$IMP_PATH = HOTEL_PIC;
+			$sql1 = "SELECT `sb_hotel_id`,`sb_hotel_name`,`sb_hotel_country`,sb_hotel_description,
+				`sb_hotel_city`,`sb_hotel_state`, `sb_hotel_zipcode`,
+				`sb_hotel_address`,`sb_hotel_phone`,`sb_hotel_star`,
+				`sb_hotel_category`,`sb_hotel_created_on`,`sb_hotel_website`,
+				CONCAT('$IMP_PATH',`sb_hotel_pic`) as `sb_hotel_pic`,
+				`sb_hotel_owner`, `sb_property_built_month` ,`sb_hotel_email`,`sb_property_built_year`,`sb_property_open_year`,
+				`is_active`
+				FROM sb_hotels WHERE `sb_hotel_id` = '$sb_hotel_id';";
+			$query = $this->db->query($sql1);
+			$hotel = $query->result_array();
+			if(count($hotel) == 0)
+			{
+				$hotelInfo =array();
+			}
+			else
+			{
+				$hotelInfo = $hotel[0];
+			}
+			unset($custData[0]['sb_guest_terms']);
+			if(count($roomNumbers)>0)
+			{
+				$custData[0]['sb_guest_allocated_room_no'] = $roomNumbers;
+				$result = array(
+					"userInfo" => $custData[0],
+					"services" => $service,
+					"hotelInfo" => $hotelInfo,
+					"hasBooked" => "1"
+					);
+			}
+			else
+			{
+				$result = array(
+					"services" => $service,
+					"hotelInfo" => $hotelInfo,
+					"hasBooked" => "0"
+					);
+			}
+			return $result;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
 	function guest_deviceToken($cdt_token, $cdt_deviceType ,$cdt_macid, $sb_hotel_guest_booking_id)
 	{
 		$sql = "SELECT * FROM `sb_guest_devicetoken` WHERE `cdt_macid` = '$cdt_macid' AND `sb_hotel_guest_booking_id` = '$sb_hotel_guest_booking_id';";
@@ -112,12 +204,12 @@ class User_model extends CI_Model
 		$device = $query->result_array();
 		if(count($device)>0)
 		{
-			$sql = "UPDATE `sb_guest_devicetoken` SET `cdt_token` = '$cdt_token' 
+			$sql = "UPDATE `sb_guest_devicetoken` SET `cdt_token` = '$cdt_token'
 					 WHERE `cdt_macid` = '$cdt_macid' AND `sb_hotel_guest_booking_id` = '$sb_hotel_guest_booking_id';";
 		}
 		else
 		{
-			$sql = "INSERT INTO `sb_guest_devicetoken` (`cdt_id`, `cdt_token`, `cdt_deviceType`, `sb_hotel_guest_booking_id`, `cdt_macid`, `created_on`) 
+			$sql = "INSERT INTO `sb_guest_devicetoken` (`cdt_id`, `cdt_token`, `cdt_deviceType`, `sb_hotel_guest_booking_id`, `cdt_macid`, `created_on`)
 					VALUES (NULL, '$cdt_token', '$cdt_deviceType', '$sb_hotel_guest_booking_id', '$cdt_macid', CURRENT_TIMESTAMP);";
 		}
 		$query = $this->db->query($sql);
@@ -126,12 +218,12 @@ class User_model extends CI_Model
 
 	function get_reservation($sb_guest_firstName, $sb_guest_lastName , $sb_guest_email, $sb_hotel_name , $sb_guest_contact_no)
 	{
-		$qry = "Select b.sb_guest_reservation_code 
+		$qry = "Select b.sb_guest_reservation_code
 				from sb_hotel_guest_bookings as b
 				JOIN sb_hotels as h on b.sb_hotel_id = h.sb_hotel_id
 				where b.sb_guest_firstName = '$sb_guest_firstName'
-				AND b. sb_guest_lastName = '$sb_guest_lastName' 
-				AND b.sb_guest_email = '$sb_guest_email' 
+				AND b. sb_guest_lastName = '$sb_guest_lastName'
+				AND b.sb_guest_email = '$sb_guest_email'
 				AND h.sb_hotel_name = '$sb_hotel_name'
 				AND b.sb_guest_contact_no = '$sb_guest_contact_no'";
 		$query = $this->db->query($qry);
@@ -140,7 +232,7 @@ class User_model extends CI_Model
 
 	public function get_hotel($sb_hotel_name)
 	{
-		if ($sb_hotel_name == '') 
+		if ($sb_hotel_name == '')
 		{
 			$qry = "Select sb_hotel_id, sb_hotel_name from sb_hotels where is_active='1';";
 			$query = $this->db->query($qry);
@@ -150,13 +242,13 @@ class User_model extends CI_Model
 			$qry = "Select sb_hotel_id, sb_hotel_name from sb_hotels where sb_hotel_name LIKE '%$sb_hotel_name%' AND  is_active='1';";
 			$query = $this->db->query($qry);
 		}
-		
+
 		return $query->result_array();
 
 	}
-	
+
 	/* Method get Hotel Name
-	 * inside system 
+	 * inside system
 	 * @param @string
 	 * return @string on success and False on Fail
 	 */
@@ -170,8 +262,8 @@ class User_model extends CI_Model
 
 	function check_reservation($sb_guest_email, $sb_hotel_id)
 	{
-		$qry = "SELECT * FROM `sb_hotel_guest_bookings` 
-				WHERE `sb_hotel_id` = '{$sb_hotel_id}' 
+		$qry = "SELECT * FROM `sb_hotel_guest_bookings`
+				WHERE `sb_hotel_id` = '{$sb_hotel_id}'
 				AND `sb_guest_email` = '{$sb_guest_email}'
 				AND `is_checkedout` = '0'";
 		$query = $this->db->query($qry);
@@ -180,8 +272,8 @@ class User_model extends CI_Model
 
 	function check_visitor($visitor_email, $sb_hotel_id)
 	{
-		$qry = "SELECT * FROM `sb_visitor` 
-				WHERE `visitor_email` = '{$visitor_email}' 
+		$qry = "SELECT * FROM `sb_visitor`
+				WHERE `visitor_email` = '{$visitor_email}'
 				AND `sb_hotel_id` = '{$sb_hotel_id}'";
 		$query = $this->db->query($qry);
 		return $query->result_array();
@@ -205,11 +297,11 @@ class User_model extends CI_Model
 		//$sb_hotel_id =$new_visitor['sb_hotel_id'];
 
 		$IMP_PATH = PARENT_SERVICE_PIC;
-		
+
 		$sql1 = "SELECT `sb_parent_service_id`,`sb_parent_service_name`,
 					CONCAT('$IMP_PATH',`sb_parent_service_image`) as `sb_parent_service_image`,`sb_parent_service_color`,
-					`sb_parent_service_created_on` 
-					FROM `sb_hotel_parent_services` 
+					`sb_parent_service_created_on`
+					FROM `sb_hotel_parent_services`
 					WHERE `sb_parent_service_id` in(SELECT distinct(`sb_parent_service_id`) FROM `sb_hotel_service_map` WHERE `sb_hotel_id` = '$sb_hotel_id')";
 		$query = $this->db->query($sql1);
 		$services = $query->result_array();
@@ -221,7 +313,7 @@ class User_model extends CI_Model
 		{
 			$service = $services;
 		}
-		
+
 		$IMP_PATH = HOTEL_PIC;
 		$sql1 = "SELECT `sb_hotel_id`,`sb_hotel_name`,`sb_hotel_country`,sb_hotel_description,
 				`sb_hotel_city`,`sb_hotel_state`, `sb_hotel_zipcode`,
@@ -229,7 +321,7 @@ class User_model extends CI_Model
 				`sb_hotel_category`,`sb_hotel_created_on`,`sb_hotel_website`,
 				CONCAT('$IMP_PATH',`sb_hotel_pic`) as `sb_hotel_pic`,
 				`sb_hotel_owner`, `sb_property_built_month` ,`sb_hotel_email`,`sb_property_built_year`,`sb_property_open_year`,
-				`is_active` 
+				`is_active`
 				FROM sb_hotels WHERE `sb_hotel_id` = '$sb_hotel_id';";
 		$query = $this->db->query($sql1);
 		$hotel = $query->result_array();
@@ -258,14 +350,14 @@ class User_model extends CI_Model
 	{
 		$this->db->where('sb_hotel_guest_booking_id', $sb_hotel_guest_booking_id);
 		$this->db->where('cdt_macid', $cdt_macid);
-		$this->db->update('sb_guest_devicetoken', $updateData); 
+		$this->db->update('sb_guest_devicetoken', $updateData);
 		return 1;
 	}
 
 	public function register_visitor($insert_arr)
 	{
 		$val = $this->db->insert('sb_hotel_guest_bookings',$insert_arr);
-		if ($val) 
+		if ($val)
 		{
 			$sb_hotel_guest_booking_id = $this->db->insert_id();
 			return $sb_hotel_guest_booking_id;
@@ -276,4 +368,4 @@ class User_model extends CI_Model
 		}
 	}
 }
-?>	
+?>
